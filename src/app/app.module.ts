b4bdd86ff1b48d53c715d561a9d4115738cbbf87
Ali Padron <alijesus29@gmail.com>
2020-03-17 11:16:30 -0400

    Chat layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,21 @@ import { AngularFireModule } from "@angular/fire";
 import { AngularFireAuthModule } from "@angular/fire/auth";
 import { AngularFirestoreModule } from "@angular/fire/firestore";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { environment } from "src/environments/environment";
+import { ChatComponent } from "./components/chat/chat.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ChatComponent],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/chat/chat.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-chat",
+  templateUrl: "./chat.component.html",
+  styles: []
+})
+export class ChatComponent implements OnInit {
+  mensaje: string;
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  enviarMensaje() {
+    console.log(this.mensaje);
+    this.mensaje = null;
+  }
+}
