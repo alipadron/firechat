698eb6aad6cf5a654daf7e1dbfe6f5079bca8a01
Ali Padron <alijesus29@gmail.com>
2020-03-17 15:05:57 -0400

    Configuration for build.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBMRI5VDbTkg35r7mRj7hYfCMMa_e5ZRus",
+    authDomain: "firechat-8317d.firebaseapp.com",
+    databaseURL: "https://firechat-8317d.firebaseio.com",
+    projectId: "firechat-8317d",
+    storageBucket: "firechat-8317d.appspot.com",
+    messagingSenderId: "1013503394608",
+    appId: "1:1013503394608:web:3a92053a68a2271ebb7ef5",
+    measurementId: "G-57NMCGMLST"
+  }
 };
